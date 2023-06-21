371b109a1f8271ee6ff8c18778fdd47c3d4a1694
mannmanjeet988 <120735535+mannmanjeet988@users.noreply.github.com>
2023-06-21 13:12:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+    let divElement=   document.getElementById("browser-info");
+         let browserName = navigator.appName;
+        let  version = navigator.appVersion;
+		divElement.value= "You are using " + browserName + "version " + version;
+		
